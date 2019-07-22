857e855a187487847375b7b4e6204c3cf7771181
Harmandeep Singh <harmans@microsoft.com>
2019-07-22 14:34:31 +0530

    Auth part 1

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -2,6 +2,7 @@ import authReducer from "./authReducer";
 import workerReducer from "./workerReducer";
 import { combineReducers } from "redux";
 import { firestoreReducer } from "redux-firestore";
+import { firebaseReducer } from "react-redux-firebase";
 
 /* CombinerReducer is creating object where the state in Store will having 2 property auth & worker 
     pointing to different reducers */
@@ -9,7 +10,8 @@ import { firestoreReducer } from "redux-firestore";
 const rootReducer = combineReducers({
   auth: authReducer,
   worker: workerReducer,
-  firestore: firestoreReducer
+  firestore: firestoreReducer,
+  firebase: firebaseReducer
 });
 
 export default rootReducer;
